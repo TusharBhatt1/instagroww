6ec6e112f894c209bfc26c68c0b8ee1227ae19ba
Tushar Bhatt <tusharbhatt0135@gmail.com>
2023-07-29 20:41:03 +0530

    ssr

--- pages/user/[user].js
+++ pages/user/[user].js
@@ -1,73 +1,63 @@
-import React, { useEffect, useState } from 'react';
-import { useRouter } from 'next/router';
+import React from 'react';
 import Navbar from '@/Components/navbar';
-import {CiLocationOn} from "react-icons/ci"
+import { CiLocationOn } from 'react-icons/ci';
 import UserPhotos from '@/Components/userPhotos';
 
-
-export default function UserDetailsPage() {
-  const router = useRouter();
-  const { query } = router;
-
-  console.log(query.user);
-  const [userDetails, setUserDetails] = useState({});
-  const [isLoading, setIsLoading] = useState(true);
-
-  const fetchUserDetails = async () => {
-    try {
-      const res = await fetch(
-        `https://api.unsplash.com/users/${query.user}/?client_id=XU9cZEBmAX1tTiRkymikGIQ4ny6zfOHwn_qgXHX_8aE`
-      );
-      const finalRes = await res.json();
-      setUserDetails(finalRes);
-      console.log(userDetails)
-      setIsLoading(false)
-    } catch (error) {
-      console.error('Error fetching data:', error);
-      setIsLoading(false); // Set loading to false on error as well
-    }
-  };
-
-  useEffect(() => {
-    if (query.user) {
-      fetchUserDetails();
-    }
-  }, [query.user]); // Add query.user to the dependency array to re-fetch when it changes
-
-  if (isLoading) {
-    return <div>Loading...</div>;
-  }
-
+export default function UserDetailsPage({ userDetails }) {
   return (
     <div>
       <Navbar />
       <div>
-
-      <div style={{display:"flex", alignItems:"center"}}>
-      
-        <img style={{borderRadius:"50%"}} src={userDetails.profile_image.large} alt={query.user} />
-       <span style={{flex:"1",display:"flex", justifyContent:"space-around"}}>
-        <h4>Following  {userDetails.following_count}</h4>
-        <h4>Followers  {userDetails.followers_count}</h4>
-        </span>
-      </div>
-        <h2>
-          {userDetails.name}  
-        </h2>
-  
-        <div style={{display:'flex', gap:"2px", marginTop:"10px"}}>
-        <CiLocationOn/>
-        <span>{userDetails.location ? <span> {userDetails.location} </span> : <span>World</span> }</span>
+        <div style={{ display: 'flex', alignItems: 'center' }}>
+          <img
+            style={{ borderRadius: '50%' }}
+            src={userDetails.profile_image.large}
+            alt={userDetails.username}
+          />
+          <span style={{ flex: '1', display: 'flex', justifyContent: 'space-around' }}>
+            <h4>Following {userDetails.following_count}</h4>
+            <h4>Followers {userDetails.followers_count}</h4>
+          </span>
         </div>
-        <p style={{padding:"4px", borderBottom:"1px solid black"}} >{userDetails.bio}</p>
-
-        <p style={{fontFamily:"cursive", fontSize:"12px"}}>Total pictures: {userDetails.total_photos}</p>
-        
-        <UserPhotos username={query.user} />
-     
-
-        
+        <h2>{userDetails.name}</h2>
+        <div style={{ display: 'flex', gap: '2px', marginTop: '10px' }}>
+          <CiLocationOn />
+          <span>
+            {userDetails.location ? <span> {userDetails.location} </span> : <span>World</span>}
+          </span>
+        </div>
+        <p style={{ padding: '4px', borderBottom: '1px solid black' }}>{userDetails.bio}</p>
+        <p style={{ fontFamily: 'cursive', fontSize: '12px' }}>
+          Total pictures: {userDetails.total_photos}
+        </p>
+        <UserPhotos username={userDetails.username} />
       </div>
     </div>
   );
+}
+
+export async function getServerSideProps(context) {
+  const { query } = context;
+  const user = query.user;
+
+  try {
+    const res = await fetch(
+      `https://api.unsplash.com/users/${user}/?client_id=XU9cZEBmAX1tTiRkymikGIQ4ny6zfOHwn_qgXHX_8aE`,{
+        headers: {
+          'Cache-Control': 'no-cache, no-store, max-age=60, must-revalidate',
+        },
+      }
+    );
+    const userDetails = await res.json();
+    return {
+      props: { userDetails },
+    };
+  } catch (error) {
+    console.error('Error fetching data:', error);
+    return {
+      props: {
+        userDetails: {}, // Return an empty object in case of an error
+      },
+    };
+  }
 }
