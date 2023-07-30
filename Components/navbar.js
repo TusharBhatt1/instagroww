ed263e8bab5982de264ad43749be498bd65548d2
Tushar Bhatt <tusharbhatt0135@gmail.com>
2023-07-30 12:25:35 +0530

    color

--- Components/navbar.js
+++ Components/navbar.js
@@ -2,6 +2,7 @@ import React from 'react'
 import Link from 'next/link'
 import styles from "@/styles/Navbar.module.css"
 import { useSelector } from 'react-redux'
+
 export default function Navbar() {
 
 let savedPostcount = useSelector(state=>state.data.length)
@@ -11,7 +12,7 @@ console.log(savedPostcount)
   return (
     <div className={styles.navbar} >
     <div className={styles.navbarContent}>
-        <h3><Link className='link' href={"/"}>InstaGroww</Link></h3>
+        <h3><Link className='link' href={"/"}>Insta<span className={styles.gro}>Gro</span><span className={styles.ww}>ww</span></Link></h3>
         <Link className='link' href={"/creator"} >Creator</Link>
         <Link className='link' href={"/saved"}>{savedPostcount === 0 ? "Saved" : `Saved (${savedPostcount})`}</Link>
         </div> 
