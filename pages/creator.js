7d746b8fed8546be24a5ae9bac00621401f7a96b
Tushar Bhatt <tusharbhatt0135@gmail.com>
2023-07-30 16:37:13 +0530

    final

--- pages/creator.js
+++ pages/creator.js
@@ -1,10 +1,13 @@
 import React from 'react'
 import myself from "@/public/asset/my.webp"
+import Image from 'next/image'
+import Img from '@/LazyLoadImage/lazyLoadImage'
 export default function creator() {
 
   return (
-    <div>
-      <img src={myself}height={"100px"} />
-    </div>
+    <div className='flex-class' style={{height:"100vh",flexDirection:"column", textAlign:"center",gap:"20px"}} >
+    <Image alt='Tushar' src={myself} style={{height:"100px",width:"100px", borderRadius:"50%"}}/>
+    <h2>Tushar Bhatt <p style={{fontSize:"12px",color:"aqua"}}>Front End Engineer</p> </h2> 
+   </div>
   )
 }
