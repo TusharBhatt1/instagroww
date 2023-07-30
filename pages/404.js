3bc1163814f8cc4f2826327961f7aabb44a01a86
Tushar Bhatt <tusharbhatt0135@gmail.com>
2023-07-30 16:03:29 +0530

    404

---
+++ pages/404.js
@@ -0,0 +1,9 @@
+import React from 'react'
+
+export default function Error() {
+  return (
+    <div className='flex-class' style={{height:"100vh"}} >
+     <p>Page Not Found</p> 
+    </div>
+  )
+}
